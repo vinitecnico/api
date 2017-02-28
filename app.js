cea28a3becb433ca10204552de8eb7d48449c6de
vinitecnico <vinitecnico@gmail.com>
2017-02-27 22:17:05 -0300

    first commit

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ let app = require('express')();
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+  next();
+});
 
 let timeApi = require('./api/timeApi')(app);
 
