3c844e1aa73dc6341c86dd78888fb9f56b6a36de
vinicius rodrigues <vinitecnico@gmail.com>
2017-03-16 19:18:32 -0300

    Update timeApi.js

--- api/timeApi.js
+++ api/timeApi.js
@@ -7,37 +7,37 @@ const Q = require('q');
 
 module.exports = {
 
-//     app.get('/api/time/:date', function (req, res) {
-//         var date = req.param('date');
-//         const timeMiddleware = new TimeMiddleware();
-//         timeMiddleware.get(date).then(function (response) {
-//             res.status(200).json(response);
-//         }).catch(function (e) {
-//             res.status(500, {
-//                 error: e
-//             });
-//         });
-//     });
+    app.get('/api/time/:date', function (req, res) {
+        var date = req.param('date');
+        const timeMiddleware = new TimeMiddleware();
+        timeMiddleware.get(date).then(function (response) {
+            res.status(200).json(response);
+        }).catch(function (e) {
+            res.status(500, {
+                error: e
+            });
+        });
+    });
 
-//     app.post('/api/time', function (req, res) {
-//         const token = req.body.token || null;
-//         tokenHelper.validate(app, token).then(response => {
-//             if (response) {
-//                 const date = req.body.date;
-//                 const timeMiddleware = new TimeMiddleware();
-//                 timeMiddleware.post(date).then(function (response) {
-//                     res.status(200).json(response);
-//                 }).catch(function (e) {
-//                     res.status(500, {
-//                         error: e
-//                     });
-//                 });
-//             } else {
-//                 return res.status(403).send({
-//                     success: false,
-//                     message: 'No token provided.'
-//                 });
-//             }
-//         });
-//     });
+    app.post('/api/time', function (req, res) {
+        const token = req.body.token || null;
+        tokenHelper.validate(app, token).then(response => {
+            if (response) {
+                const date = req.body.date;
+                const timeMiddleware = new TimeMiddleware();
+                timeMiddleware.post(date).then(function (response) {
+                    res.status(200).json(response);
+                }).catch(function (e) {
+                    res.status(500, {
+                        error: e
+                    });
+                });
+            } else {
+                return res.status(403).send({
+                    success: false,
+                    message: 'No token provided.'
+                });
+            }
+        });
+    });
 };
