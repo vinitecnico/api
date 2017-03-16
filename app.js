f0b49ff676923606d3c8a801a60c32186eb0b312
vinicius rodrigues <vinitecnico@gmail.com>
2017-03-16 18:11:48 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
+
+
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const config = require('./config');
+const responseFormat = require('./helpers/responseFormatHelper');
+const tokenHelper = require('./helpers/tokenHelper');
 
-app.set('port', (process.env.PORT || 5000));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('tokenSecret', config.tokenSecret);
+
 
 app.get('/', function(request, response) {
   response.send('test blá - blá - blá');
 });
+
+app.set('port', (process.env.PORT || 5000));
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
