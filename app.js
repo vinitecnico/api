051b8a9a318a18c32fb546628198f509b3f7f68b
vinicius rodrigues <vinitecnico@gmail.com>
2017-03-16 18:58:56 -0300

    Update app.js

--- app.js
+++ app.js
@@ -19,22 +19,22 @@ app.use(function (req, res, next) {
   next();
 });
 
-let timeApi = require('./api/timeApi')(app);
+//let timeApi = require('./api/timeApi')(app);
 
 app.get('/', function (req, res) {
    res.send('timeApi! method - get http://localhost:3000/api/time/20170228 - post http://localhost:3000/api/time {"date": "20170228"}');
 });
 
-// app.post('/api/authenticate', function (req, res) {
-//   if (config.user == req.body.user && config.pwd == req.body.pwd) {
-//     const token = tokenHelper.create(app);    
-//     res.status(200).json({ success: true, message: 'Enjoy your token!', token: token });
-//     console.log('token:' + token);
-//   } else {
-//     res.status(500).json(responseFormat.error('Authentication failed. User not found.'));
-//     console.log('Authentication failed. User not found.');
-//   }
-// });
+app.post('/api/authenticate', function (req, res) {
+  if (config.user == req.body.user && config.pwd == req.body.pwd) {
+    const token = tokenHelper.create(app);    
+    res.status(200).json({ success: true, message: 'Enjoy your token!', token: token });
+    console.log('token:' + token);
+  } else {
+    res.status(500).json(responseFormat.error('Authentication failed. User not found.'));
+    console.log('Authentication failed. User not found.');
+  }
+});
 
 app.set('port', (process.env.PORT || 5000));
 
