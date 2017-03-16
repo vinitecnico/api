f262d6a45c1a154bc4472b651c8867749a7194d2
vinicius rodrigues <vinitecnico@gmail.com>
2017-03-16 18:14:49 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,17 +4,27 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const config = require('./config');
-const responseFormat = require('./helpers/responseFormatHelper');
-const tokenHelper = require('./helpers/tokenHelper');
+// const responseFormat = require('./helpers/responseFormatHelper');
+// const tokenHelper = require('./helpers/tokenHelper');
 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set('tokenSecret', config.tokenSecret);
+// app.set('tokenSecret', config.tokenSecret);
 
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+  next();
+});
 
 app.get('/', function(request, response) {
   response.send('test blá - blá - blá');
+});
+
+app.get('/', function (req, res) {
+   res.send('timeApi! method - get http://localhost:3000/api/time/20170228 - post http://localhost:3000/api/time {"date": "20170228"}');
 });
 
 app.set('port', (process.env.PORT || 5000));
