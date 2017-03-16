eba62564bcbafdd7283c961dd274e63ac85f8fe0
vinicius rodrigues <vinitecnico@gmail.com>
2017-03-16 18:28:43 -0300

    Update tokenHelper.js

--- helpers/tokenHelper.js
+++ helpers/tokenHelper.js
@@ -1,4 +1,6 @@
-const jwt = require('jsonwebtoken');
+'use strict';
+
+// const jwt = require('jsonwebtoken');
 const config = require('../config');
 const Q = require('q');
 
@@ -7,17 +9,17 @@ class tokenHelper {
         return jwt.sign(config, app.get('tokenSecret'));
     }
 
-    static validate(app, token) {
-        const defer = Q.defer();
-        jwt.verify(token, app.get('tokenSecret'), function (err, decoded) {
-            if (err) {
-                defer.resolve(false);
-            } else {
-                defer.resolve(true);
-            }
-        });
-        return defer.promise;
-    }
+//     static validate(app, token) {
+//         const defer = Q.defer();
+//         jwt.verify(token, app.get('tokenSecret'), function (err, decoded) {
+//             if (err) {
+//                 defer.resolve(false);
+//             } else {
+//                 defer.resolve(true);
+//             }
+//         });
+//         return defer.promise;
+//     }
 }
 
 module.exports = tokenHelper;
