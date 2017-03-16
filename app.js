0960b6cbc1dbc4ce35e662740a8216a72de59056
vinicius rodrigues <vinitecnico@gmail.com>
2017-03-16 18:57:58 -0300

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(function (req, res, next) {
   next();
 });
 
-// let timeApi = require('./api/timeApi')(app);
+let timeApi = require('./api/timeApi')(app);
 
 app.get('/', function (req, res) {
    res.send('timeApi! method - get http://localhost:3000/api/time/20170228 - post http://localhost:3000/api/time {"date": "20170228"}');
